0370fcb46cc65592c232b88fc6c094ab2e45e73e
Taylor Bisset <87632154+TaylorBisset@users.noreply.github.com>
2024-03-21 13:30:30 -0700

    02 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+let repeatedString = "";
+const repeatString = function(string, number) {
+    for (let i = 0; i < number; i++) {
+        repeatedString = repeatedString + string;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
