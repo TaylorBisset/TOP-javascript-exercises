520c111e1037bf3f2ed8865f5b762a723b771213
Taylor Bisset <87632154+TaylorBisset@users.noreply.github.com>
2024-03-21 13:46:34 -0700

    03-reverseString complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+let reversedString = "";
+const reverseString = function(wordToReverse) {
+    for (let i = wordToReverse.length - 1; i >= 0; i--) {
+        reversedString = reversedString + wordToReverse.charAt(i);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
