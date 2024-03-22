17e8f76a4375ecb8a125d5ff55cc50a61a84ad84
Taylor Bisset <87632154+TaylorBisset@users.noreply.github.com>
2024-03-22 11:12:41 -0700

    Add pseudocode for newArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, argument) {
+    const newArray = [];
+    /*
+    if (item != argument) {
+        newArray.push(item);
+    }
+    return newArray;
+    */
 };
 
 // Do not edit below this line
