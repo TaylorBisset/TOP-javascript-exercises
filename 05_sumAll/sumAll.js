a212d39b365b2875cffc24b63dd23ca5d133fcd7
Taylor Bisset <87632154+TaylorBisset@users.noreply.github.com>
2024-03-25 10:15:53 -0700

    05-sumAll complete. Used for loop.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sumOfAll = 0;
+    for (i = num1; i <= num2; i++) {
+        sumOfAll += i;
+    }
+    return sumOfAll;
 };
 
 // Do not edit below this line
