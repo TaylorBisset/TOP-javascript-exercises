61176efd37059c42d32d203eb97e5d31656e0817
Taylor Bisset <87632154+TaylorBisset@users.noreply.github.com>
2024-03-26 15:55:35 -0700

    handle negative number

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,13 @@
 const repeatString = function(string, number) {
     let repeatedString = "";
-    for (let i = 0; i < number; i++) {
-        repeatedString = repeatedString + string;
-    }
-    return repeatedString;
+    if (number >= 0) {
+        for (let i = 0; i < number; i++) {
+            repeatedString = repeatedString + string;
+        }
+        return repeatedString
+    } else {
+        return "ERROR";
+    } 
 };
 
 // Do not edit below this line
