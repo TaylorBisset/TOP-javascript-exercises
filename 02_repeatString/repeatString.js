eb0ab3dc658fa154a3489e5393e802ef4999693d
Taylor Bisset <87632154+TaylorBisset@users.noreply.github.com>
2024-03-26 15:45:26 -0700

    4 tests passed. failed ERROR test.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,5 @@
-let repeatedString = "";
 const repeatString = function(string, number) {
+    let repeatedString = "";
     for (let i = 0; i < number; i++) {
         repeatedString = repeatedString + string;
     }
